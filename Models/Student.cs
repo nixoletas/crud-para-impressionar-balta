ca8d9f1b62684e6f8f9c14642539d97686c11c45
nixoletas <nicholetas@gmail.com>
2025-07-04 13:38:06 -0300

    Create Student and Subscription models

---
+++ Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace crud_pra_impressionar_balta.Models;
+
+public class Student
+{
+    [Key]
+    [Display(Name = "Id")]
+    public long Id { get; set; }
+    
+    [Required(ErrorMessage = "Informe o nome")]
+    [StringLength(80, ErrorMessage = "O nome não deve ser maior que 80 caracteres")]
+    [MinLength(5, ErrorMessage = "O nome deve conter pelo menos caracteres")]
+    [Display(Name = "Nome Completo")]
+    public string Name { get; set; } =  string.Empty;
+    
+    [Required(ErrorMessage = "Informe o email")]
+    [EmailAddress(ErrorMessage = "E-mail inválido")]
+    [Display(Name = "E-mail")]
+    public string Email { get; set; } = string.Empty;
+    
+    public List<Subscription> Subscriptions { get; set; } = new();
+}
---
+++ Models/Subscription.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+
+namespace crud_pra_impressionar_balta.Models;
+
+public class Subscription
+{
+    [Key]
+    [DisplayName("Id")]
+    public int Id { get; set; }
+    
+    [Required(ErrorMessage = "Informe o título da assinatura")]
+    [StringLength(80, ErrorMessage = "O título deve conter até 80 caracteres")]
+    [MinLength(5, ErrorMessage = "O título deve conter pelo menos 5 caracteres")]
+    [DisplayName("Título")]
+    public string Title { get; set; } = string.Empty;
+    
+    [DataType(DataType.DateTime)]
+    //[GreaterThanToday]
+    [DisplayName("Início")]
+    public DateTime StartDate { get; set; }
+    
+    [DataType(DataType.DateTime)]
+    [DisplayName("Término")]
+    public DateTime EndDate { get; set; }
+    
+    [DisplayName("Aluno")]
+    [Required(ErrorMessage = "Aluno inválido")]
+    public int StudentId { get; set; }
+    
+    public Student? Student { get; set; }
+    
+}
